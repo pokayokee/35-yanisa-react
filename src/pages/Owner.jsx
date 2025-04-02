4af8f4bcdf0e62664036a666c6e10aa19c51b964
Yanisa <kadyanisa@gmail.com>
2025-04-02 21:47:01 +0700

    change img src

--- src/pages/Owner.jsx
+++ src/pages/Owner.jsx
@@ -4,7 +4,7 @@ const Owner = () => {
   return (
     <div className='min-h-screen flex flex-col items-center bg-white'>
       <h1 className='text-3xl font-bold mt-4'>Pakkad - Group E - 35</h1>
-      <img src="./public/asset/lettuce.jpg" alt="" className='w-1/4 rounded-lg my-8' />
+      <img src="https://media.istockphoto.com/id/1876910251/vector/cabbage-vegetable-animated-cartoon-icon-vector-in-hand-drawing-doodle-illustration.jpg?s=612x612&w=0&k=20&c=PrwnY5TkoVhYYQYuUhgslRl-1NWSC7RcIh8cn257V9U=" alt="" className='w-1/4 rounded-lg my-8' />
       <h3 className='font-bold'>Short Biography:</h3>
       <p>I've been learning Junior Software Developer</p>
     </div>
